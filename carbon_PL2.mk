c8ba3ac8f86252fded7a52a08a272118a50838ae
kiam001 <blablatupfen@gmail.com>
2020-12-09 19:59:24 +0100

    Update carbon_PL2.mk

--- carbon_PL2.mk
+++ carbon_PL2.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common carbon stuff
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 #nherit from PL2 device
 $(call inherit-product, device/nokia/PL2/device.mk)
